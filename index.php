6d4b117704c9b0602c329e9738117ae9044ca903
Tanishq Sharma <tanishqsh@gmail.com>
2016-01-09 22:23:02 +0530

    Elegant Maintenance Page

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+  <title> Binary Caves </title>
+  <link href="binary.css" rel="stylesheet" type="text/css"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href='https://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
+</head>
+<body>
+  <div class="maincontainer">
+    <div class="container">
+      <img src="BinaryPrimaryLogotransparent.png" width="200px" height="200px"/>
+      <p class="comingsoontext"> Binary Caves </p>
+    </div>
+  </div>
+  </body
+</html>
